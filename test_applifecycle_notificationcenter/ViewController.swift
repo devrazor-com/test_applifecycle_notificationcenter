e2dcccb59bef025d7f770981a8b5e73d1bffbab4
Victor Balas <vbalas1@rogers.com>
2020-07-14 14:06:35 -0400

    First commit

--- test_applifecycle_notificationcenter/ViewController.swift
+++ test_applifecycle_notificationcenter/ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @objc func didEnterBackground(notification:NSNotification) {
+        
+        print("ViewController " + #function)
+    
+    }
+    
+    @objc func willEnterForeground(notification:NSNotification) {
+        
+        print("ViewController " + #function)
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if #available(iOS 13.0, *) {
+            NotificationCenter.default.addObserver(self, selector: #selector(didEnterBackground), name: UIScene.didEnterBackgroundNotification, object: nil)
+        } else {
+            NotificationCenter.default.addObserver(self, selector: #selector(didEnterBackground), name: UIApplication.didEnterBackgroundNotification, object: nil)
+        }
+        
+        if #available(iOS 13.0, *) {
+            NotificationCenter.default.addObserver(self, selector: #selector(willEnterForeground), name: UIScene.willEnterForegroundNotification, object: nil)
+        } else {
+            NotificationCenter.default.addObserver(self, selector: #selector(willEnterForeground), name: UIApplication.willEnterForegroundNotification, object: nil)
+        }
+        
+        
     }
 
 
