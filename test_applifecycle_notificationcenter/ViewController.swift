0e053d72ea41a155fc2d06a1e2edbaba5714916b
Victor Balas <vbalas1@rogers.com>
2020-07-14 13:40:29 -0400

    Initial Commit

---
+++ test_applifecycle_notificationcenter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test_applifecycle_notificationcenter
+//
+//  Created by Victor on 2020-07-14.
+//  Copyright © 2020 devrazor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
